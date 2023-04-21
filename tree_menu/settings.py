0074471cf966e2a7bae5450b1af57e2feb333432
Elena Maksimova <emaksimo@c2r9s2.42yerevan.am>
2023-04-22 01:02:13 +0400

    readme

--- tree_menu/settings.py
+++ tree_menu/settings.py
@@ -9,14 +9,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-	'mptt',
-	'tree_menu',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'mptt',
+	'tree_menu',
 ]
 
 MIDDLEWARE = [
